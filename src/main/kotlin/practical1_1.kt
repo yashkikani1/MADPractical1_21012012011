a024f460d632580bf63a0e7a2baa0e5d4170b522
yashkikani1 <110801454+yashkikani1@users.noreply.github.com>
2022-08-23 21:26:37 +0530

    Initial commit

---
+++ src/main/kotlin/practical1_1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var int:Int=100
+    val float:Float=200.95F
+    val char:Char='a'
+    val string:String="Hello World"
+    val boolean:Boolean=true
+    val double:Double=45.56
+    val long:Long=12345
+    val short:Short=-7
+    val byte:Byte=127
+    println("Integer Value: $int")
+    println("Float Value: $float")
+    println("Character Value: $char")
+    println("String Value: $string")
+    println("Boolean Value: $boolean")
+    println("Double Value: $double")
+    println("Long Value: $long")
+    println("Short Value: $short")
+    println("Byte Value: $byte")
+}
